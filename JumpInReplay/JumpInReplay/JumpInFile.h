85e6f6308956d31d2aa1e690d340dda1ec3f57f2
Atomus48 <102902941+Atomus48@users.noreply.github.com>
2023-03-26 09:39:05 +0200

    Deleted old version

--- JumpInReplay/JumpInReplay/JumpInFile.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class JumpInFile
-{public:
-	void SaveFile(std::string path);
-	void ReadFile(std::string path);
-};
-
--- JumpInReplay/JumpInReplay/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_PATCH 1
-#define VERSION_BUILD 3
-
-#define stringify(a) stringify_(a)
-#define stringify_(a) #a
-
